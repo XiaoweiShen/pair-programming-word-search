6232ec0395bf7b54dfaee0cc6a1c0ee05839beb7
Wei <snhiui@gmail.com>
2022-09-06 22:54:45 +0000

    pair prog with Jodie and trying refactor

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,3 +1,6 @@
+// test/test_wordsearch.js
+// pair-programming-word-search with Wei Shen
+
 const chai = require('chai');
 const assert = chai.assert;
 
@@ -35,4 +38,58 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'A', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'T', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CAT')
+
+    assert.isTrue(result);
+  });
+
+  it("should return undefined if the word matrix is empty",() => {
+    const result = wordSearch([])
+    assert.strictEqual(result, undefined);
+  });
+
+  it("should return true if the word appears backwards horizontally in the word matrix",() => {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'A', 'T', 'U', 'A', 'L'],
+      ['S', 'E', 'A', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'T', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'TAC')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word appears backwards vertically in the word matrix", () => {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'D', 'D', 'U', 'A', 'L'],
+      ['S', 'E', 'A', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'T', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'TAC')
+    
+    assert.isTrue(result);
+  });
+
 });
